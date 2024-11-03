232cb411e680c2f8c01c3c563d50fb9c04e85565
Fabrizio Mejia <fmejiaa@csu.fullerton.edu>
2024-11-03 13:49:12 -0800

    Initial Commit

---
+++ csuf-navigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  csuf-navigator
+//
+//  Created by Fabrizio Mejia on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ csuf-navigator/csuf_navigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  csuf_navigatorApp.swift
+//  csuf-navigator
+//
+//  Created by Fabrizio Mejia on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct csuf_navigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
